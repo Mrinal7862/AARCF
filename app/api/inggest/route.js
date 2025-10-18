ddd993e94a30257e0a597c0a3ee20bed36548a65
customrc98-alt <customrc98@gmail.com>
2025-10-19 03:36:28 +0530

    anotherOne

--- app/api/inggest/route.js
+++ app/api/inggest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/ingest";
 
-// Create an API that serves zero functions
+// Create an API that serves zero functions to the inngest
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
